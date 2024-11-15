43597f491fb422fba1d64e2a8c930178005cb7a1
LouisDevo <louis.speelman@devoteam.com>
2024-11-15 15:26:22 +0100

    SA name should be email, even though variable says name

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,6 +66,13 @@ data "google_iam_policy" "tldd-pdf-bucket" {
   }
 }
 
+data "google_iam_policy" "tldd-pdf-secret" {
+  binding {
+    role = "roles/secretmanager.secretAccessor"
+    members = ["serviceAccount:${google_service_account.tldd.email}"]
+  }
+}
+
 resource "google_storage_bucket_iam_policy" "tldd-pdf-bucket-policy" { 
   bucket = google_storage_bucket.tldd.name
   policy_data = data.google_iam_policy.tldd-pdf-bucket.policy_data
