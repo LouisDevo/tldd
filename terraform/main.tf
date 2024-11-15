886cd89c0faff0ad55871558abd0e4f410219a86
LouisDevo <louis.speelman@devoteam.com>
2024-11-15 14:53:48 +0100

    policy_data, not policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ data "google_iam_policy" "tldd-pdf-bucket" {
 
 resource "google_storage_bucket_iam_policy" "tldd-pdf-bucket-policy" { 
   bucket = google_storage_bucket.tldd.name
-  policy_data = data.google_iam_policy.tldd-pdf-bucket.policy
+  policy_data = data.google_iam_policy.tldd-pdf-bucket.policy_data
 }
 
 resource "google_cloud_run_service" "tldd" {
